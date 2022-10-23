b308f7c5a767cb561da4d1ddfa39f47c65d53a0b
Luis C. Berrocal <luis.berrocal@payjoy.com>
2022-10-23 14:55:32 -0500

    Basic printing of root node.

--- flake8_django_plugin/ast_example.py
+++ flake8_django_plugin/ast_example.py
@@ -1,4 +1,12 @@
+import ast
 from pathlib import Path
 
 
-def parse_models_file(filename:Path):
+def parse_models_file(filename: Path):
+    with open(filename) as model_file:
+        code = model_file.read()
+    node = ast.parse(code)
+
+    print(node)
+    print('-' * 120)
+    print(node.body)
